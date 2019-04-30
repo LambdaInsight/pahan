fe9ebb2ec53dbd49609ab7da10df30c76b26a719
Istvan Szukacs <istvan@lambdainsight.com>
2019-04-30 22:31:54 +0000

    Adding listing Imapala queries

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 echo "This script assumes pip being installed"
-pip3 install --user --upgrade virtualenv
+pip install --user --upgrade virtualenv
 virtualenv venv
 . venv/bin/activate
 if [[ -z "$VIRTUAL_ENV" ]]; then
   echo "No VIRTUAL_ENV set"
 else
   echo "VIRTUAL_ENV is set"
-  pip3 install -r requirements.txt
+  pip install -r requirements.txt
 fi
